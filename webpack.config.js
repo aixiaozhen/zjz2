debbdad224bc7d709977b0e9d1df6a5e829e56d4
aixiaozhen <26857137@qq.com>
2018-02-26 14:09:52 +0800

    构建Vue项目的环境结束

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,85 @@
-const path = require('path')
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-module.exports={
-  entry:{
-    app:'./app/js/main.js'
-  },
-  module:{
-    loaders:[
-      {
-        test:/\.html$/,//表示识别文件的正则表达式
-        loader:'html-loader'
-      },
-      {
-        test:/\.vue$/,
-        loader:'vue-loader'
-      },
-      {
-        test:/\.scss$/,
-        loader:'style-loader!css-loader!sass-loader'
-      }
+module.exports = (env) => {
+    if (!env) {
+        env = {}
+    }
+    const plugins = [
+        new CleanWebpackPlugin(["dist"]),
+        new HtmlWebpackPlugin({
+            title: "Development",
+            template: "./app/views/index.html",
+        }),
+
     ]
-  },
-  plugins:[],
-  output:{
-    filename:'[name].min.js',
-    path:path.resolve(__dirname,'dist')
-  }
+    if (env.production) {
+        plugins.push(
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: "production",
+                },
+            }),
+            new ExtractTextPlugin("style.css")
+        )
+    }
+
+    return {
+        entry: {
+            app: "./app/js/main.js",
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            compress: true,
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/, // 表示识别文件的正则表达式
+                    loader: "html-loader",
+                },
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                    options: {
+                        cssModules: {
+                            localIdentName: "[path][name]---[local]---[hash:base64:5]",
+                            camelCase: true,
+                        },
+                        loaders: env.production ? {
+                            css: ExtractTextPlugin.extract({
+                                use: "css-loader!px2rem-loader?remUnit=75&remPrecision=8",
+                                fallback: "vue-style-loader", // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                            }),
+                            scss: ExtractTextPlugin.extract({
+                                use: "css-loader!px2rem-loader?remUnit=75&remPrecision=8!sass-loader",
+                                fallback: "vue-style-loader", // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+                            }),
+                        } : {
+                            css: "vue-style-loader!css-loader!px2rem-loader?remUnit=75&remPrecision=8",
+                            scss: "vue-style-loader!css-loader!px2rem-loader?remUnit=75&remPrecision=8!sass-loader",
+                        },
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    loader: "style-loader!css-loader!sass-loader",
+                },
+            ],
+        },
+        plugins,
+        output: {
+            filename: "[name].min.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.esm.js", // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+            },
+        },
+    }
 }
